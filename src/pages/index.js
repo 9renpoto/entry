aaa86fcb8a63a634b3c29b02f5ce069317b082ad
Keisuke Kan <9renpoto@gmail.com>
2017-08-05 14:08:25 +0900

    fix(src) remove unused

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,30 @@
-import React from "react"
-import Link from "gatsby-link"
-import get from "lodash/get"
-import Helmet from "react-helmet"
+/* @flow */
+import React, { Component } from 'react'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
 
-import Bio from "../components/Bio"
-import { rhythm } from "../utils/typography"
+import { rhythm } from '../utils/typography'
 
-class BlogIndex extends React.Component {
-  render() {
-    // console.log("props", this.props)
+declare var graphql: any // TODO remove
+type Props = {
+  data: any
+}
+
+export default class BlogIndex extends Component {
+  props: Props
+  render () {
     const pageLinks = []
-    const siteTitle = get(this, "props.data.site.siteMetadata.title")
-    const posts = get(this, "props.data.allMarkdownRemark.edges")
+    const posts = this.props.data.allMarkdownRemark.edges
     posts.forEach(post => {
-      if (post.node.path !== "/404/") {
-        const title = get(post, "node.frontmatter.title") || post.node.path
+      if (post.node.path !== '/404/') {
         pageLinks.push(
           <li
             key={post.node.frontmatter.path}
             style={{
-              marginBottom: rhythm(1 / 4),
+              marginBottom: rhythm(1 / 4)
             }}
           >
-            <Link style={{ boxShadow: "none" }} to={post.node.frontmatter.path}>
+            <Link style={{ boxShadow: 'none' }} to={post.node.frontmatter.path}>
               {post.node.frontmatter.title}
             </Link>
           </li>
@@ -32,8 +34,7 @@ class BlogIndex extends React.Component {
 
     return (
       <div>
-        <Helmet title={get(this, "props.data.site.siteMetadata.title")} />
-        <Bio />
+        <Helmet title={this.props.data.site.siteMetadata.title} />
         <ul>
           {pageLinks}
         </ul>
@@ -41,12 +42,6 @@ class BlogIndex extends React.Component {
     )
   }
 }
-
-BlogIndex.propTypes = {
-  route: React.PropTypes.object,
-}
-
-export default BlogIndex
 
 export const pageQuery = graphql`
   query IndexQuery {
