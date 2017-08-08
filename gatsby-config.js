738b706d15a7b3f3c1589ff7d44db51b72e4ac05
Keisuke Kan <9renpoto@gmail.com>
2017-08-09 01:17:17 +0900

    chore(gatsby-config) add ga id

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-39548809-3`
       }
     },
     `gatsby-plugin-offline`,
