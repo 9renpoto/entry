ec3a3877e82c4eedd97f70db8ea88d2e08152bce
Keisuke Kan <9renpoto@gmail.com>
2017-09-03 06:43:19 +0900

    chore(package) update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,7 +88,7 @@ module.exports = {
             query: `
             {
               allMarkdownRemark(
-                limit: 1000,
+                limit: 1,
                 sort: { order: DESC, fields: [frontmatter___date] }
               ) {
                 edges {
