45b4178a414aca67479b8f183704fa8dffdcefcf
Keisuke Kan <9renpoto@gmail.com>
2017-10-08 11:19:46 +0900

    refactor(src) tiny fix

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,5 +1,5 @@
 /* @flow */
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import { Header } from '@9renpoto/style'
 import Link from 'gatsby-link'
 import 'prismjs/themes/prism-solarizedlight.css'
@@ -8,16 +8,17 @@ type Props = {
   children: Function
 }
 
-export default class Template extends Component<Props> {
+export default class Template extends PureComponent<Props> {
   render () {
     const { children } = this.props
     return (
       <div>
         <Header
-          Logo={className =>
+          Logo={className => (
             <Link className={className} to='/'>
               {':-)'}
-            </Link>}
+            </Link>
+          )}
           titles={['slides', 'profile']}
           baseUrl={'https://9renpoto.github.io'}
         />
